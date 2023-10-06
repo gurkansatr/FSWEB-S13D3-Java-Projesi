106f130abb884b7711f662c48f3fbedfd57994de
gurkansatr <gurkan-satir@hotmail.com>
2023-10-06 22:59:42 +0300

    s13d3cevaplar

---
+++ src/com/workintech/model/Main.java
@@ -0,0 +1,24 @@
+package com.workintech.model;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("John", "Doe", 20);
+
+        System.out.println("Firstname: " + person.getFirstName());
+
+        System.out.println("LastName: " + person.getLastName());
+
+        System.out.println("Age: " + person.getAge());
+
+        System.out.println(person.salary);
+        Wall wall = new Wall(5,4);
+        System.out.println("------------------------------------------");
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+        System.out.println("------------------------------------------");
+    }
+
+}
